db4ff28cd3c8aa1dd9f262452f2159f4bd4d61df
Jaspal99 <Khalsajaspalsingh6@gmail.com>
2026-06-20 23:06:07 +0530

    framework apiview and response

--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -2,48 +2,42 @@ from django.shortcuts import render
 from .models import Person
 from .serializers import PersonModelSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse,JsonResponse
+# from django.http import Response
 import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
-@csrf_exempt
+@api_view(["GET","PATCH","PUT"])
 def singleobj(request,id):
     data = get_object_or_404(Person,id=id)
     if request.method == "PUT":
-        json = request.body
-        stream = io.BytesIO(json)
-        parsed_data = JSONParser().parse(stream)
-        serializer = PersonModelSerializer(data,data=parsed_data)
+        serializer = PersonModelSerializer(data,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({"updated":"successfully"},status=status.HTTP_200_OK)
-        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response({"updated":"successfully"},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     if request.method == "PATCH":
-        json = request.body
-        stream = io.BytesIO(json)
-        parsed_data = JSONParser().parse(stream)
-        serializer = PersonModelSerializer(data,data=parsed_data,partial=True)
+        serializer = PersonModelSerializer(data,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({"updated":"successfully"},status=status.HTTP_200_OK)
-        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response({"updated":"successfully"},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     serializer = PersonModelSerializer(data)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
-@csrf_exempt
+@api_view(["GET","PATCH","PUT"])
 def multipleobj(request):
     if request.method == "POST":
-        json = request.body
-        stream = io.BytesIO(json)
-        parsed_data = JSONParser().parse(stream)
-        serializer = PersonModelSerializer(data=parsed_data)
+        serializer = PersonModelSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({"created":"successfully"},status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response({"created":"successfully"},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     data = Person.objects.all()
     serializer = PersonModelSerializer(data,many=True)
-    return JsonResponse(serializer.data,safe=False)
+    return Response(serializer.data)
