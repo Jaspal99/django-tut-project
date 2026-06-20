70cf6d6964f162cf1eb66595c971627224090abd
Jaspal99 <Khalsajaspalsingh6@gmail.com>
2026-06-20 18:48:35 +0530

    deserilizer with create

--- peoplehub/api/serializers.py
+++ peoplehub/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-
+from .models import Person
 class PersonSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=200)
     age = serializers.IntegerField()
     city = serializers.CharField(max_length=150)
+
+    def create(self,validated_data):
+        return Person.objects.create(**validated_data)
--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 
 # Create your views here.
 def singleobj(request):
     data = Person.objects.get(id=1)
     serializer = PersonSerializer(data)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data,content_type='application/json')
+    return JsonResponse(serializer.data)
 
 @csrf_exempt
 def multipleobj(request):
@@ -20,8 +20,11 @@ def multipleobj(request):
         json = request.body
         stream = io.BytesIO(json)
         parsed_data = JSONParser().parse(stream)
-        print(parsed_data)
+        serializer = PersonSerializer(data=parsed_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({"created":"successfully"},status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     data = Person.objects.all()
     serializer = PersonSerializer(data,many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data,content_type='application/json')
+    return JsonResponse(serializer.data,safe=False)
